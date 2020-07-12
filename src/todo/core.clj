46ae10011e92d5ee9883b529fefc04c3c3d64a7f
Prasanna Gnanaraj <prasanna.gnanaraj@alphasights.com>
2020-07-12 22:53:17 +0100

    add created_at ability

--- src/todo/core.clj
+++ src/todo/core.clj
@@ -4,16 +4,28 @@
 
 (def file-location (System/getenv "TODO_LOCATION"))
 
+(defn now [] (new java.util.Date))
+
 (defn add-content
   "appends content to todo file"
   [file-location text-content]
   (with-open [file (clojure.java.io/writer file-location :append true)]
-    (.write file (str (first text-content) "\n"))))
+    (.write file (str (first text-content) "\t" (now) "\n"))))
 
+(defn line-to-row
+  "Converts line content to a row obj"
+  [line-content]
+  (let [splitter (fn [string] (clojure.string/split string #"\t"))
+        split-line (map #(clojure.string/trim %) (splitter line-content))
+        time (second split-line)]
+    
+    {:todo (first split-line) :time (if (nil? time) "UNKNOWN" time)}))
+
+                                        ;TODO refactor below function
 (defn textarray-to-table
   "Converts text array to object format for pretty printing"
   [content-array]
-  (reduce #(conj %1 {:todos %2}) [] content-array))
+  (reduce #(conj %1 (line-to-row %2)) [] content-array))
 
 (defn read-content
   "reads content from todo file"
--- test/todo/core_test.clj
+++ test/todo/core_test.clj
@@ -4,4 +4,9 @@
 
 (deftest textarray-to-table-test
   (testing "Converts text array to tabular format"
-    (is (= (textarray-to-table '("hello" "hi there")) [{:todos "hello"} {:todos "hi there"}]))))
+    (is (= (textarray-to-table '("hello" "hi there"))
+           [{:todo "hello" :time "UNKNOWN"} {:todo "hi there" :time "UNKNOWN"}]))))
+
+(deftest line-to-row-test
+  (testing "Converts line content to a row obj")
+  (is (= (line-to-row "hello \t Today") {:todo "hello" :time "Today"})))
